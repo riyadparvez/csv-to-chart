65ceb0380c640c3c36d81c268242e1c4ffb6460a
Riyad Parvez <social.riyad@gmail.com>
2013-03-08 22:51:46 +0600

    Fixed for very small range

---
+++ GraphGenerator/DoubleUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GraphGenerator
+{
+    static class DoubleUtils
+    {
+        public static double HalfFloor(double num)
+        {
+            double threshold = (int)num + 0.5;
+            if (num > threshold)
+            {
+                return Math.Floor(num);
+            }
+            else
+            {
+                return threshold;
+            }
+        }
+    }
+}
